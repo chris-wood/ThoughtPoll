0797d771924bfa313fb414d56be51719ac84002c
Christopher Wood <christopherwood07@gmail.com>
2012-11-18 17:15:17 -0500

    Added Express template - next step is to set up MongooseDB connection

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-// First extend the express server's prototype
-require('./lib/config');
 
-var debug = require('debug')('app'),
-	server = require('./lib/server'),
-	db = require('./lib/db'),
-	handler = require('./lib/handler')(db);
+/**
+ * Module dependencies.
+ */
 
-// Setup routes
-require('./lib/router')(server, handler);
+var express = require('express')
+  , routes = require('./routes')
+  , mongoose = require('mongoose')
+  , connect = require('connect')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-// All set, start listening!
-server.listen(3000);
-debug("Express server listening on port %d in %s mode", server.address().port, process.env.NODE_ENV);
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
