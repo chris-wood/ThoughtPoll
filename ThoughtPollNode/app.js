cc2280b6210dcc72e735f8d55b0f34811f7de5e3
U-caw-rhino\caw <caw@caw-rhino.(none)>
2013-02-04 01:02:47 -0500

    Added Jade layout

--- ThoughtPollNode/app.js
+++ ThoughtPollNode/app.js
@@ -2,4 +2,14 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-// TODO: continue the rest...
+app.set('view engine', 'jade');
+app.set('view options', { layout : true});
+app.set('views', __dirname + '/views');
+
+app.get('/?', function(req, res) {
+	res.render('index');
+});
+
+var port = 8000;
+app.listen(port);
+console.log('Listening on port ' + port);
