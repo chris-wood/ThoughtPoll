6da89e610e4d655655d2db4051a11c960d76e1ec
Christopher Wood <christopherwood07@gmail.com>
2013-02-27 00:21:00 -0500

    Added some content to the database model - need to go in and separate into MVC components

--- src/model/db.js
+++ src/model/db.js
@@ -2,12 +2,26 @@
 
 var mongoose = require( 'mongoose' );
 
-var teamSchema = new mongoose.Schema({
- country: String,
- GroupName: String
+var pollSchema = new mongoose.Schema({
+	id : { type: Number, required: true },
+	question : { type: String, required: true },
+	startTime : Date,
+	endTime : Date
 });
-mongoose.model( 'Team', teamSchema );
+mongoose.model('Poll', pollSchema);
 
+var answerSchema = new mongoose.Schema({
+	id : { type: Number, required: true },
+	pollId : { type: Number, required: true },
+	answer : { type: String, required: true }
+});
+mongoose.model('Answer', answerSchema);
 
+var voteSchema = new mongoose.Schema({
+	id : { type: Number, required: true },
+	answerId : { type: Number, required: true }
+	// TODO: maybe add 
+});
+mongoose.model('Vote', voteSchema);
 
 mongoose.connect( 'mongodb://localhost/thoughtpoll' );
