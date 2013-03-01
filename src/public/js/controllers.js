200ce6245b6b9d19485c2f642a4388d0ed401443
Christopher Wood <christopherwood07@gmail.com>
2013-03-01 12:04:26 -0500

    Made some changes to ThoughtPoll

--- src/public/js/controllers.js
+++ src/public/js/controllers.js
@@ -3,16 +3,24 @@
 /* Controllers */
 
 function AppCtrl($scope, $http) {
-  $http({method: 'GET', url: '/api/name'}).
-  success(function(data, status, headers, config) {
-    $scope.name = data.name;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!'
-  });
+  	$http({method: 'GET', url: '/api/name'}).
+  		success(function(data, status, headers, config) {
+    		$scope.name = data.name;
+  		}).
+  		error(function(data, status, headers, config) {
+    		$scope.name = 'Error!'
+  		});
+
+  	$scope.pollQuestions = Array();
+	$scope.pollQuestions['q1'] = "question 1";
+	$scope.pollQuestions['q2'] = "question 2";
+	$scope.pollQuestions['q3'] = "question 3";
+	$scope.pollQuestions['q4'] = "question 4";
 }
 
-function PollController() {}
+function PollController($scope, $http) {
+	// TODO
+}
 PollController.$inject = [];
 
 function HistoryController() {}
@@ -20,3 +28,5 @@ HistoryController.$inject = [];
 
 function AboutController() {}
 AboutController.$inject = [];
+
+
