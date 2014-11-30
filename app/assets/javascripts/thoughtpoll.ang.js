da00f10d770c5e9c5dba6180d4070f03639d4b69
Christopher Wood <christopherwood07@gmail.com>
2014-11-30 08:38:31 -0800

    cleanup

--- app/assets/javascripts/thoughtpoll.ang.js
+++ app/assets/javascripts/thoughtpoll.ang.js
@@ -17,97 +17,97 @@ tp.controller('TPQuestionController', ['$scope', '$http', function($scope, $http
 		});
 	};
 
-  $scope.submitVote = function(qid, aindex) {
-  	var aid = $scope.question.poll_answers[aindex].id;
-  	var vote = {
-  		question_id : qid,
-  		answer_id : aid,
-  		answer_index: aindex
-  	};
+	$scope.submitVote = function(qid, aindex) {
+		var aid = $scope.question.poll_answers[aindex].id;
+		var vote = {
+			question_id : qid,
+			answer_id : aid,
+			answer_index: aindex
+		};
 
-  	$http.post('/vote.json', vote).
-  		success(function(data, status, headers, config) {
-  			$scope.answered = true;
-  			$scope.questionHeader = "HERE'S WHAT THE WORLD THINKS";
+		$http.post('/vote.json', vote).
+			success(function(data, status, headers, config) {
+				$scope.answered = true;
+				$scope.questionHeader = "HERE'S WHAT THE WORLD THINKS";
 
-  			// Now plot the data for the user...
-  			$scope.plotBarChart(data);
-  		}).
-  		error(function(data, status, headers, config) {
-  			alert("failed to submit vote");
-  		});
-  };
-
-  $scope.plotBarChart = function(results) {
-
-  	// Generate the list of labels
-  	var ls = [];
-  	var dd = [];
-  	for (var i = 0; i < $scope.question.poll_answers.length; i++) {
-  		ls.push($scope.question.poll_answers[i].body);
-  		dd.push(0); // initialize the results array to 0
-  	}
-
-  	console.log(results);
-  	for (var i = 0; i < results.length; i++) {
-  		if (results[i].answer_index == null) {
-  			dd[0] = dd[0] + 1;
-  		} else {
-  			var index = results[i].answer_index;
-  			dd[index] = dd[index] + 1;
-  		}
-  	}
-
-  	var data = {
-	    labels: ls,
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            fillColor: "rgba(220,220,220,0.5)",
-	            strokeColor: "rgba(220,220,220,0.8)",
-	            highlightFill: "rgba(220,220,220,0.75)",
-	            highlightStroke: "rgba(220,220,220,1)",
-	            // data: [65, 59, 80, 81]
-	            data: dd
-	        }
-	    ]
+				// Now plot the data for the user...
+				$scope.plotBarChart(data);
+			}).
+			error(function(data, status, headers, config) {
+				alert("failed to submit vote");
+			});
 	};
 
-	var options = {
-	    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-	    scaleBeginAtZero : true,
+	$scope.plotBarChart = function(results) {
 
-	    //Boolean - Whether grid lines are shown across the chart
-	    scaleShowGridLines : true,
+		// Generate the list of labels
+		var ls = [];
+		var dd = [];
+		for (var i = 0; i < $scope.question.poll_answers.length; i++) {
+			ls.push($scope.question.poll_answers[i].body);
+			dd.push(0); // initialize the results array to 0
+		}
 
-	    //String - Colour of the grid lines
-	    scaleGridLineColor : "rgba(0,0,0,.05)",
+		console.log(results);
+		for (var i = 0; i < results.length; i++) {
+			if (results[i].answer_index == null) {
+				dd[0] = dd[0] + 1;
+			} else {
+				var index = results[i].answer_index;
+				dd[index] = dd[index] + 1;
+			}
+		}
 
-	    //Number - Width of the grid lines
-	    scaleGridLineWidth : 1,
+		var data = {
+			labels: ls,
+			datasets: [
+				{
+					label: "My First dataset",
+					fillColor: "rgba(220,220,220,0.5)",
+					strokeColor: "rgba(220,220,220,0.8)",
+					highlightFill: "rgba(220,220,220,0.75)",
+					highlightStroke: "rgba(220,220,220,1)",
+					data: dd
+				}
+			]
+		};
 
-	    //Boolean - If there is a stroke on each bar
-	    barShowStroke : true,
+		var options = {
+			//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+			scaleBeginAtZero : true,
 
-	    //Number - Pixel width of the bar stroke
-	    barStrokeWidth : 2,
+			//Boolean - Whether grid lines are shown across the chart
+			scaleShowGridLines : true,
 
-	    //Number - Spacing between each of the X value sets
-	    barValueSpacing : 5,
+			//String - Colour of the grid lines
+			scaleGridLineColor : "rgba(0,0,0,.05)",
 
-	    //Number - Spacing between data sets within X values
-	    barDatasetSpacing : 1,
+			//Number - Width of the grid lines
+			scaleGridLineWidth : 1,
 
-	    //String - A legend template
-	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+			//Boolean - If there is a stroke on each bar
+			barShowStroke : true,
 
-	};
+			//Number - Pixel width of the bar stroke
+			barStrokeWidth : 2,
 
-	var ctx = document.getElementById("barChart").getContext("2d");
-	var myBarChart = new Chart(ctx).Bar(data, options);
-  }
+			//Number - Spacing between each of the X value sets
+			barValueSpacing : 5,
 
-  // Load the data
-  $scope.getQOTD();
+			//Number - Spacing between data sets within X values
+			barDatasetSpacing : 1,
 
+			//String - A legend template
+			// legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+		};
+
+		var ctx = document.getElementById("barChart").getContext("2d");
+		var myBarChart = new Chart(ctx).Bar(data, options);
+	}
+
+	//////////////////
+	/// -- MAIN -- ///
+	//////////////////
+
+	$scope.getQOTD();
 }]);
