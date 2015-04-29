0fe7389cf88bf0d5c438bf9bc0c9a3415f074d26
Christopher Wood <christopherwood07@gmail.com>
2015-04-28 18:26:10 -0700

    rawr

---
+++ thoughtpoll-server/server.js
@@ -0,0 +1,33 @@
+// Import required modules.
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+// Initialize our Express app.
+var app = express();
+
+var qotdURI = '/qotd';
+
+// Configure Stormpath.
+var stormpathMiddleware = stormpath.init(app, {
+  apiKeyFile: '/Users/robert/.stormpath/apiKey.properties',
+  application: 'https://api.stormpath.com/v1/applications/3ow3opGnLNWjwhTYQwd014',
+  secretKey: 'some_long_random_string',
+  expandCustomData: true,
+  enableForgotPassword: true
+});
+
+app.use(stormpathMiddleware);
+
+// Generate a simple home page.
+app.get('/', function(req, res) {
+  res.redirect(qotdURI);
+});
+
+// Generate a simple dashboard page.
+app.get(qotdURI, stormpath.loginRequired, function(req, res) {
+  res.send('Hi: ' + req.user.email + '. Logout <a href="/logout">here</a>');
+});
+
+// Listen for incoming requests and serve them.
+app.listen(process.env.PORT || 3000);
+
